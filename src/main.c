42fa211ec4062ec3c9e8be0ee790edbd41345427
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-02-02 15:08:55 +0100

    Update main.c
    Latest stable version

--- src/main.c
+++ src/main.c
@@ -1,52 +1,77 @@
-#include <stdio.h>
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
 #include <SDL3/SDL.h>
-#include "../include/sort_algorithms.h"
+#include <pthread.h>
+#include "../include/sorting_algorithms.h"
+
 
 /*
 
-   compile with gcc src/main.c -o bin/app -I include -L lib -lmingw32 -lSDL3
+   compile with gcc src/main.c src/sorting_algorithms.c -o bin/app -I include -L lib -lmingw30 -lSDL3 
 
 */
 
+#define WIN_W 1280
+#define WIN_H 640
 
-#define ARR_SIZE 100 
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
 
-int main(int argc, char* argv[]) {
-	const int version = SDL_GetVersion();
+ushort arr[10] = {4, 8, 3, 5, 10, 7, 2, 9, 1, 6};
 
-	SDL_Log("SDL %d.%d.%d", SDL_VERSIONNUM_MAJOR(version), SDL_VERSIONNUM_MINOR(version), SDL_VERSIONNUM_MICRO(version));
-
-
-    SDL_Window *window;
-    bool loop = false;
-
-    SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow(
-        "Sorting Algorithms Visualizer",
-		640,
-		320,
-		SDL_WINDOW_OPENGL
-	);
-
-    if (window == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
-        return 1;
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Error initializing SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    while (!loop) {
-        SDL_Event event;
+    window = SDL_CreateWindow("Sorting Algorithms Visualizer", WIN_W, WIN_H, SDL_WINDOW_OPENGL);
 
-		SDL_RenderLine(SDL_Renderer *renderer, 10, 10, 20, 20);
-
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
-                loop = true;
-            }
-        }
+    if (!window) {
+        SDL_Log("Error creating window: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
+    renderer = SDL_CreateRenderer(window, NULL);
+
+    if (!renderer) {
+        SDL_Log("Error creating renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }	
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    SDL_DestroyRenderer(renderer);
+    renderer = NULL;
     SDL_DestroyWindow(window);
-    SDL_Quit();
+    window = NULL;
+    SDL_QuitSubSystem(SDL_INIT_VIDEO);
+}
 
-    return 0;
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    } else if (event->type == SDL_EVENT_KEY_DOWN) {
+		if (event->key.scancode == SDL_SCANCODE_SPACE) {
+			sortInsertion(arr, 10);
+		}
+	}
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+	SDL_SetRenderDrawColor(renderer, 100, 100, 100, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+	
+	for (int i = 0; i < 10; ++i) {
+		SDL_RenderLine(renderer, 10 + i * 10, WIN_H, 10 + i * 10, WIN_H - arr[i] * 60);
+	}
+
+	SDL_RenderPresent(renderer);
+
+	return SDL_APP_CONTINUE;
 }
