57e654ad0ff46fc1798b42ba6e11303def973da4
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-02-05 13:46:01 +0100

    Update sort_algorithms.c

--- src/sort_algorithms.c
+++ src/sort_algorithms.c
@@ -1,40 +1,34 @@
-#include "../include/sort_algorithms.h"
+#include "../include/sorting_algorithms.h"
+#include "../include/utils.h"
 
-void swap(ushort *a, ushort *b) {
-	ushort temp = *a;
-	*a = *b;
-	*b = temp;
+void sortInsertion(Ushort arr[], Ushort size) {
+        for (Ushort i = 1; i < size; ++i) {
+                Ushort currentElement = arr[i];
+                short currentIndex = i - 1;
+
+                while (currentIndex >= 0 && currentElement < arr[currentIndex]) {
+                        arr[currentIndex + 1] = arr[currentIndex];
+                        currentIndex--;
+                }
+
+                arr[currentIndex + 1] = currentElement;
+        }
 }
 
-void sortInsertion(ushort arr[], ushort len) {
-	for (ushort i = 1; i < len; ++i) {
-		ushort currentElement = arr[i];
-		short currentIndex = i - 1;
+void sortSelection(Ushort arr[], Ushort size) {
+        Ushort mini, index;
 
-		while (currentIndex >= 0 && currentElement < arr[currentIndex]) {
-			arr[currentIndex + 1] = arr[currentIndex];
-			currentIndex--;
-		}
+        for (Ushort i = 0; i < size - 1; ++i) {
+                index = i;
+                mini = arr[index];
 
-		arr[currentIndex + 1] = currentElement;
-	}
+                for (Ushort j = i + 1; j < size; ++j) {
+                        if (arr[j] < mini) {
+                                mini = arr[j];
+                                index = j;
+                        }
+                }
+
+                swap(&arr[i], &arr[index]);
+        }
 }
-
-void sortSelection(ushort arr[], ushort len) {
-	ushort mini, index;
-
-	for (ushort i = 0; i < len - 1; ++i) {
-		index = i;
-		mini = arr[index];
-
-		for (ushort j = i + 1; j < len; ++j) {
-			if (arr[j] < mini) {
-				mini = arr[j];
-				index = j;
-			}
-		}
-
-		swap(&arr[i], &arr[index]);
-	}
-}
-
