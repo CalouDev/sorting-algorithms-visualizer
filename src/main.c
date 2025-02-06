1f4fd9572dfa63b7303b21c5717f89e70cbe2090
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-02-06 18:19:33 +0100

    Update main.c
    Added button

--- src/main.c
+++ src/main.c
@@ -1,29 +1,24 @@
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_ttf.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
 #include "../include/sorting_algorithms.h"
 #include "../include/utils.h"
-
-
-/*
-
-   gcc src/main.c src/sorting_algorithms.c src/utils.c -o bin/app -I include -L lib -lmingw32 -lSDL3 -mwindows
-
-*/
+#include "../include/components.h"
 
 /*
 
-	Slow sorting algos	
+   gcc src/main.c src/sorting_algorithms.c src/utils.c -o bin/app -I include -L lib -lmingw32 -lSDL3 -lSDL3_ttf -mwindows
 
 */
 
 #define WIN_W 1280
 #define WIN_H 640
-#define INTERVALLE 100 // ms
-#define ARR_SIZE 127
+#define INTERVALLE 75 // ms
+#define ARR_SIZE 100
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
@@ -32,26 +27,26 @@ Ushort arr[ARR_SIZE];
 
 Uint64 lastTime;
 
-void (*sortingFunctions[2])(Ushort[], Ushort, Ushort) = {sortInsertion, sortInsertion};
+void (*sortingFunctions[2])(Ushort[], Ushort, Ushort) = {sortInsertion, sortSelection};
 
 bool sorting = false;
-Ushort indexInsertion = 1;
+Ushort indexSorting;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
 	srand(time(NULL));
 
 	// Filling the array
-	
+
 	for (Ushort i = 1; i <= ARR_SIZE; ++i)
 		arr[i-1] = i;
 
 	shuffle(arr, ARR_SIZE);
 
 	// MENU 
-	
+
 
 	/*SDL_Log("Use ./app -l or ./app --list to get the index of each sorting algorithms");
-	
+
 	if (argc > 1) {
 		if (strcmp(argv[1], "-l") == 0 || strcmp(argv[1], "--list") == 0) {
 			SDL_Log("Menu d'affichage");
@@ -69,6 +64,11 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
         SDL_Log("Error initializing SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
+	if (!TTF_Init()) {
+		SDL_Log("Error initializing TTF: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
 
     window = SDL_CreateWindow("Sorting Algorithms Visualizer", WIN_W, WIN_H, SDL_WINDOW_OPENGL);
 
@@ -95,6 +95,8 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     SDL_DestroyWindow(window);
     window = NULL;
     SDL_QuitSubSystem(SDL_INIT_VIDEO);
+	SDL_Quit();
+	TTF_Quit();
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
@@ -102,6 +104,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
         return SDL_APP_SUCCESS;
     } else if (event->type == SDL_EVENT_KEY_DOWN) {
 		if (event->key.scancode == SDL_SCANCODE_SPACE) {
+			indexSorting = 0;
 			sorting = true;
 			lastTime = SDL_GetTicks();
 
@@ -111,23 +114,28 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
+	Button* monBouton = createButton(1050, 10, 200, 50);
 	SDL_SetRenderDrawColor(renderer, 10, 10, 10, SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(renderer);
 
 	SDL_SetRenderDrawColor(renderer, 200, 200, 200, SDL_ALPHA_OPAQUE);
-	
+
 	for (Ushort i = 0; i < ARR_SIZE; ++i) {
-		SDL_RenderLine(renderer, 10 + i * 10, WIN_H, 10 + i * 10, WIN_H - arr[i] * 5);
+		SDL_RenderLine(renderer, 10 + i * 10, WIN_H, 10 + i * 10, WIN_H - arr[i] * 6);
 	}
 
 	if (sorting && SDL_GetTicks() > lastTime + INTERVALLE) {
 		lastTime = SDL_GetTicks();
-		sortingFunctions[0](arr, ARR_SIZE, indexInsertion);
-		indexInsertion++;
-		if (indexInsertion >= ARR_SIZE) sorting = false;
+		sortingFunctions[1](arr, ARR_SIZE, indexSorting);
+		indexSorting++;
+		if (indexSorting >= ARR_SIZE) sorting = false;
 	}
 
+	renderButton(renderer, monBouton);
+
 	SDL_RenderPresent(renderer);
+
+	free(monBouton);
 
 	return SDL_APP_CONTINUE;
 }
