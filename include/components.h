37708410f69af71f5ec786a19db3047b1f298d58
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-02-06 18:20:39 +0100

    Create components.h

---
+++ include/components.h
@@ -0,0 +1,16 @@
+#include <SDL3/SDL.h>
+
+#ifndef COMPONENTS
+#define COMPONENTS
+
+typedef struct {
+	SDL_FRect box;
+	SDL_Color clr;
+	bool hovered;
+	bool pressed;
+} Button;
+
+Button* createButton(float x, float y, float w, float h);
+void renderButton(SDL_Renderer* renderer, Button* but);
+
+#endif
