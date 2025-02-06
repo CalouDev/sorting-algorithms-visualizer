e72aa387a125363de0d5570de6f6e2ff1bff4ba1
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-02-06 18:32:35 +0100

    Update components.c

--- src/components.c
+++ src/components.c
@@ -1,21 +1,13 @@
 #include "../include/components.h"
 #include <stdlib.h>
 
-Button* createButton(float x, float y, float w, float h) {
-	Button* btn = malloc(sizeof(Button));
-	if (!btn) {
-		SDL_Log("Problème d'alloc de mémoire");
-		return NULL;
-	}
-
-	*btn = (Button) {
-		.box = (SDL_FRect){x, y, w, h},
-		.clr = (SDL_Color){255, 255, 255, SDL_ALPHA_OPAQUE},
-		.hovered = false,
-		.pressed = false
-	};
-
-	return btn;
+Button createButton(float x, float y, float w, float h) {
+    return (Button) {
+        .box = (SDL_FRect){x, y, w, h},
+        .clr = (SDL_Color){255, 255, 255, SDL_ALPHA_OPAQUE},
+        .hovered = false,
+        .pressed = false
+    };
 }
 
 void renderButton(SDL_Renderer* renderer, Button* but) {
