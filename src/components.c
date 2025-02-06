dded34f9ea099f23421b65bd191d29c002352190
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-02-06 19:08:50 +0100

    Update components.c

--- src/components.c
+++ src/components.c
@@ -16,3 +16,12 @@ void renderButton(SDL_Renderer* renderer, Button* but) {
 	SDL_RenderFillRect(renderer, &but->box);
 	SDL_RenderRect(renderer, &but->box);
 }
+
+bool isHovered(SDL_FRect box, float mouseX, float mouseY) {
+	return (mouseX >= box.x && mouseX <= box.x + box.w && mouseY >= box.y && mouseY <= box.y + box.h);
+}
+
+bool isPressed(SDL_MouseButtonFlags data, SDL_FRect box, float mouseX, float mouseY) {
+	return (isHovered(box, mouseX, mouseY) && data == 1);
+}
+
