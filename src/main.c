27348570779ba8a0a858ff0c276deca472862aa1
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-02-08 14:51:37 +0100

    Update main.c
    Added text to render button

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 
 /*
 
-   gcc src/main.c src/sorting_algorithms.c src/utils.c -o bin/app -I include -L lib -lmingw32 -lSDL3 -lSDL3_ttf -mwindows
+   gcc src/*.c -o bin/app -I include -L lib -lmingw32 -lSDL3 -lSDL3_ttf -mwindows
 
 */
 
@@ -47,6 +47,18 @@ SDL_MouseButtonFlags mouseData;
 float mouseX, mouseY;
 SDL_Cursor *handCursor, *defaultCursor;
 bool elementHovered = false;
+
+TTF_Font* font;
+//SDL_Surface textSurface;
+//SDL_Texture textTexture;
+const char* buffer = "ceci est un test";
+SDL_Color green = {0, 255, 0, SDL_ALPHA_OPAQUE};
+SDL_Rect rect = {10, 10, 100, 50};
+
+// Test
+
+TTF_Text bufferFinal;
+SDL_Surface* screenSurface;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
 	srand(time(NULL));
@@ -87,11 +99,19 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
 	handCursor = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_POINTER);
 	defaultCursor = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_DEFAULT);
 
+	font = TTF_OpenFont("font/sans.ttf", 25);
+
 	lastTime = SDL_GetTicks();
 
 	for (int i = 0; i < N_ALGOS; ++i) {
 		buttons[i] = createButton(1050, 10 + 60 * i, 200, 50);
 	}
+
+	screenSurface = SDL_CreateSurface(WIN_W, WIN_H, SDL_PIXELFORMAT_RGBA32);
+
+	bufferFinal = *TTF_CreateText(NULL, font, "Hello world", 12);
+	TTF_SetTextColor(&bufferFinal, 255, 255, 255, SDL_ALPHA_OPAQUE);
+	TTF_SetTextPosition(&bufferFinal, 10, 10);
 
 	return SDL_APP_CONTINUE;
 }
@@ -101,7 +121,10 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     renderer = NULL;
     SDL_DestroyWindow(window);
     window = NULL;
-    SDL_QuitSubSystem(SDL_INIT_VIDEO);
+    TTF_CloseFont(font);
+	TTF_DestroyText(&bufferFinal);
+	SDL_QuitSubSystem(SDL_INIT_VIDEO);
+	SDL_DestroySurface(screenSurface);
 	SDL_Quit();
 	TTF_Quit();
 }
@@ -122,7 +145,6 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 			if (event->button.button == SDL_BUTTON_LEFT) {
 				for (int i = 0; i < N_ALGOS; ++i) {
 					if (buttons[i].hovered) {
-						SDL_Log("Algo %d choisi : %s", i+1, strSortingFunctions[i]);
 						buttons[i].pressed = true;
 						algoChoosen = i;
 						shuffle(arr, ARR_SIZE);
@@ -139,18 +161,20 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 		case SDL_EVENT_KEY_DOWN:
 			switch (event->key.scancode) {
 				case SDL_SCANCODE_SPACE:
-					switch (algoChoosen) {
-						case 0:
-							indexSorting = 1;
-							break;
-						case 1:
-							indexSorting = 0;
-							break;
-						default:
-							break;
+					if (!sorting) {
+						switch (algoChoosen) {
+							case 0:
+								indexSorting = 1;
+								break;
+							case 1:
+								indexSorting = 0;
+								break;
+							default:
+								break;
+						}
+						lastTime = SDL_GetTicks();
+						sorting = true;
 					}
-					lastTime = SDL_GetTicks();
-					sorting = true;
 					break;
 				default:
 					break;
@@ -169,6 +193,8 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 	SDL_SetRenderDrawColor(renderer, 10, 10, 10, SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(renderer);
 
+	TTF_DrawSurfaceText(&bufferFinal, 0, 0, screenSurface);
+
 	SDL_SetRenderDrawColor(renderer, 200, 200, 200, SDL_ALPHA_OPAQUE);
 
 	for (Ushort i = 0; i < ARR_SIZE; ++i) {
@@ -183,7 +209,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 	}
 
 	for (int i = 0; i < N_ALGOS; ++i)
-		renderButton(renderer, &buttons[i]);
+		renderButton(renderer, &buttons[i], strSortingFunctions[i]);
 
 	if (elementHovered) SDL_SetCursor(handCursor);
 	else SDL_SetCursor(defaultCursor);
