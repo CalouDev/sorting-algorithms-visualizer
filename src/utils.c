c4cd9c04e720d3928a9d1cbeca20d3bb8d0c7856
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-02-13 22:39:53 +0100

    Update utils.c
    Change to dynamic arr for mergeSubArray

--- src/utils.c
+++ src/utils.c
@@ -2,22 +2,6 @@
 #include <stdlib.h>
 
 void swap(Ushort *a, Ushort *b) {
-        Ushort temp = *a;
-        *a = *b;
-        *b = temp;
-}
-
-void shuffle(Ushort arr[], Ushort size) {
-    for (int i = size - 1; i > 0; i--) {
-        int j = rand() % (i + 1);
-        swap(&arr[i], &arr[j]);
-    }
-}
-
-#include "../include/utils.h"
-#include <stdlib.h>
-
-void swap(Ushort *a, Ushort *b) {
 	Ushort temp = *a;
 	*a = *b;
 	*b = temp;
@@ -33,7 +17,8 @@ void shuffle(Ushort arr[], Ushort size) {
 void mergeSubArr(Ushort arr[], Ushort l, Ushort r, Ushort m) {
 	Ushort i, j, k;
 	Ushort leftSize = m - l + 1, rightSize = r - m;
-	Ushort leftArr[leftSize], rightArr[rightSize];
+	Ushort* leftArr = malloc(leftSize * sizeof(Ushort));
+	Ushort* rightArr = malloc(rightSize * sizeof(Ushort));
 
 	for (i = 0; i < leftSize; ++i) leftArr[i] = arr[l + i];
 	for (j = 0; j < rightSize; ++j) rightArr[j] = arr[m + j + 1];
@@ -52,4 +37,7 @@ void mergeSubArr(Ushort arr[], Ushort l, Ushort r, Ushort m) {
 
 	while (j < rightSize)
 		arr[k++] = rightArr[j++];
+
+	free(leftArr);
+	free(rightArr);
 }
