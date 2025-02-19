22a270a750e4eff9b645eede10e930da4f51c1c5
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-02-19 21:04:48 +0100

    Update sorting_algorithms.h

--- include/sorting_algorithms.h
+++ include/sorting_algorithms.h
@@ -1,11 +1,14 @@
 #ifndef SORTING_ALGORITHMS
 #define SORTING_ALGORITHMS
 
+#include <SDL3/SDL.h>
+
 typedef unsigned short Ushort;
 typedef enum {SORTING_STOP, SORTING_CONTINUE} sortingState;
 
 sortingState sortInsertion(short arr[], Ushort size, Ushort index);
 sortingState sortSelection(short arr[], Ushort size, Ushort index);
 sortingState sortBubble(short arr[], Ushort size, Ushort index);
+sortingState sortMerge(short arr[], Ushort size, Ushort index);
 
 #endif
