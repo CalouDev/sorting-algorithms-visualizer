ea092b8d61aac38c9a3b1fad4377b1a2e11bccc3
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-02-19 21:03:04 +0100

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,4 @@
 #include "../include/utils.h"
-#include <stdlib.h>
 
 void swap(Ushort *a, Ushort *b) {
 	Ushort temp = *a;
@@ -9,7 +8,7 @@ void swap(Ushort *a, Ushort *b) {
 
 void shuffle(Ushort arr[], Ushort size) {
     for (int i = size - 1; i > 0; i--) {
-        int j = rand() % (i + 1);
+        int j = SDL_rand(size) % (i + 1);
         swap(&arr[i], &arr[j]);
     }
 }
@@ -21,8 +20,8 @@ Ushort min(Ushort a, Ushort b) {
 void mergeSubArr(short arr[], Ushort l, Ushort r, Ushort m) {
 	Ushort i, j, k;
 	Ushort leftSize = m - l + 1, rightSize = r - m;
-	Ushort* leftArr = malloc(leftSize * sizeof(Ushort));
-	Ushort* rightArr = malloc(rightSize * sizeof(Ushort));
+	Ushort* leftArr = SDL_malloc(leftSize * sizeof(Ushort));
+	Ushort* rightArr = SDL_malloc(rightSize * sizeof(Ushort));
 
 	for (i = 0; i < leftSize; ++i) leftArr[i] = arr[l + i];
 	for (j = 0; j < rightSize; ++j) rightArr[j] = arr[m + j + 1];
@@ -42,6 +41,7 @@ void mergeSubArr(short arr[], Ushort l, Ushort r, Ushort m) {
 	while (j < rightSize)
 		arr[k++] = rightArr[j++];
 
-	free(leftArr);
-	free(rightArr);
+	SDL_free(leftArr);
+	SDL_free(rightArr);
 }
+
