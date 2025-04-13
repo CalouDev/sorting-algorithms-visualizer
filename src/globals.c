5ca1a2aa172c1d4cfd10fd92b0f9ba789c5cc19f
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-04-13 16:56:03 +0200

    Delete src/globals.c

--- src/globals.c
+++
@@ -1,9 +0,0 @@
-#include <SDL3/SDL_mixer.h>
-#include <SDL3/SDL_ttf.h>
-#include <time.h>
-#include "../include/components.h"
-#include "../include/globals.h"
-#include "../include/sorting_algorithms.h"
-#include "../include/utils.h"
-
-
